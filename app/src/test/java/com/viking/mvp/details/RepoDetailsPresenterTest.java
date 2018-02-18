aa5fa018e7ac3e8e553faa686caabbf512107f22
Lars Viking <lars@harbourfront.se>
2018-02-18 21:20:38 +0100

    Changed test on repo details to support rxjava

--- app/src/test/java/com/viking/mvp/details/RepoDetailsPresenterTest.java
+++ app/src/test/java/com/viking/mvp/details/RepoDetailsPresenterTest.java
@@ -1,15 +1,26 @@
 package com.viking.mvp.details;
 
+import android.support.annotation.NonNull;
+
 import com.viking.api.Repo;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Scheduler;
+import io.reactivex.Single;
+import io.reactivex.android.plugins.RxAndroidPlugins;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.internal.schedulers.ExecutorScheduler;
+import io.reactivex.plugins.RxJavaPlugins;
+
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 /**
@@ -34,24 +45,52 @@ public class RepoDetailsPresenterTest {
     public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
 
+        Scheduler immediate = new Scheduler() {
+            @Override
+            public Disposable scheduleDirect(@NonNull Runnable run, long delay, @NonNull TimeUnit unit) {
+                // this prevents StackOverflowErrors when scheduling with a delay
+                return super.scheduleDirect(run, 0, unit);
+            }
+
+            @Override
+            public Worker createWorker() {
+                return new ExecutorScheduler.ExecutorWorker(Runnable::run);
+            }
+        };
+
+        RxJavaPlugins.setInitIoSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitComputationSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitNewThreadSchedulerHandler(scheduler -> immediate);
+        RxJavaPlugins.setInitSingleSchedulerHandler(scheduler -> immediate);
+        RxAndroidPlugins.setInitMainThreadSchedulerHandler(scheduler -> immediate);
+
         mPresenter = new RepoDetailsPresenter(mView, mModel);
         mRepo = new Repo();
-
-        when(mModel.getRepo(invalidId)).thenReturn(null);
-        when(mModel.getRepo(validId)).thenReturn(mRepo);
     }
 
     @Test
     public void shouldPassOnResume() {
+        //Given
+        when(mModel.getRepo(validId)).thenReturn(Single.just(mRepo));
         mPresenter.setId(validId);
+
+        //When
         mPresenter.onResume();
-        Mockito.verify(mView).updateRepo(mRepo);
+
+        //Then
+        verify(mView).updateRepo(mRepo);
     }
 
     @Test
     public void shouldFailOnResume() {
+        //Given
+        when(mModel.getRepo(invalidId)).thenReturn(Single.error(new Throwable()));
         mPresenter.setId(invalidId);
+
+        //When
         mPresenter.onResume();
-        Mockito.verify(mView).displayError();
+
+        //Then
+        verify(mView).displayError();
     }
 }
